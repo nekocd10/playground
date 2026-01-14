99927b5932bf788d5f672ab65d4b90a44c3eb9bf
nekocd10 <nekoCd10@proton.me>
2026-01-14 16:54:25 +0000

    ih

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express' ;
+import path from 'path' ;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(process.cwd(), 'views'));
+
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index'); // looks for views/index.ejs
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.listen(PORT, () => {
+    console.log('i hope this doesnt crash on me')
+    });
